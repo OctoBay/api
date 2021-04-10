c1bdd223825dfbfb2c54cc98ec107c06e04a81bd
mktcode <kontakt@markus-kottlaender.de>
2021-04-10 17:43:08 +0200

    added discussion-by-id endpoint

---
+++ controllers/github/discussion-by-id.js
@@ -0,0 +1,35 @@
+const axios = require('axios')
+
+module.exports = (req, res) => {
+  let discussionId = req.params.discussionId
+  axios
+    .post(
+      "https://api.github.com/graphql",
+      {
+        query: `query {
+  node(id:"${discussionId}") {
+    ... on Discussion {
+      id
+      title
+      url
+    }
+  }
+}`
+      },
+      {
+        headers: {
+          Authorization: "bearer " + process.env.GITHUB_PERSONAL_ACCESS_TOKEN,
+          'GraphQL-Features': 'discussions_api'
+        }
+      }
+    )
+    .then(data => {
+      if (data.data.errors) {
+        res.status(404).json(data.data.errors)
+      } else {
+        res.json(data.data.data.node)
+      }
+    }).catch(e => {
+      res.status(500).send(JSON.stringify(e, Object.getOwnPropertyNames(e)))
+    })
+}
